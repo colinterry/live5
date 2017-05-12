15500060849ae6620642fca1680a4f133ea04951
Colin Terry <colin.terry@comset.co.uk>
2017-05-12 12:52:48 +0100

    Video 44: Added Custom control to vie

--- uita/resources/webapp/controller/View1.controller.js
+++ uita/resources/webapp/controller/View1.controller.js
@@ -4,6 +4,14 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("live5.uita.controller.View1", {
-
+		onRefresh: function() {
+			this.getView().byId("D3Chart").doRefresh();
+			this.byId("pullToRefresh").hide();
+		},
+		onSentimentSelect: function(evt) {
+			this.getView().byId("D3Chart").setSentiment(evt.getSource().getSelected());
+			this.getView().byId("D3Chart").setType("");
+			this.getView().byId("D3Chart").doRefresh();
+		}
 	});
 });
